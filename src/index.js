61f67ff98c0bfc2ed14fb312a04e97a7a8a7538a
WondrousSquirrel <wondroussquirrel@gmail.com>
2018-10-09 13:07:02 +0300

    first bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import _ from 'lodash'; //
+/**
+ * теперь lodash подгружается как сторонняя библиотека
+ * и собирается webpack`ом, а не загружается через script 
+ * в index.html  
+ * */ 
+
 function component () {
     let element = document.createElement('div');
 
