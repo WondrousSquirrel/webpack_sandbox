6472c867c04119d6f78bb804573b4d16c950553a
WondrousSquirrel <wondroussquirrel@gmail.com>
2018-10-09 14:35:03 +0300

    loading css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import _ from 'lodash'; //
 /**
  * теперь lodash подгружается как сторонняя библиотека
  * и собирается webpack`ом, а не загружается через script 
  * в index.html  
  * */ 
+import _ from 'lodash';
+import './style.css';
 
 function component () {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    element.classList.add('hello');
     return element;
 }
 
