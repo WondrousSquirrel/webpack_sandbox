3e3984ae43f37da90bc5d2f67dc2a72349b19148
WondrousSquirrel <wondroussquirrel@gmail.com>
2018-10-09 16:01:42 +0300

    two entry point

--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@
  * в index.html  
  * */ 
 import _ from 'lodash';
+import printMe from './print';
 import './Components/index_component/style.css';
 import Icon from './Components/index_component/icon.png';
 import Data from './Components/index_component/data.xml'
 
 function component () {
     let element = document.createElement('div');
+    let btn = document.createElement('button');
+
+    btn.innerHTML = 'Click me';
+    btn.onclick = printMe;
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-
+    
     // Добавление favicon`а на страницу
     document.querySelector("link[rel='shortcut icon'").href = Icon;
-
+    
     console.log(Data);
+    element.appendChild(btn);
 
     return element;
 }
