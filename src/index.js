e82dcd7046b7736aafba9d43ae52970b6d0cefd9
WondrousSquirrel <wondroussquirrel@gmail.com>
2018-10-10 14:17:22 +0300

    tree shaking

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@
  * */ 
 import _ from 'lodash';
 import printMe from './print';
+import { cube } from './math';
+
 import './Components/index_component/style.css';
 import Icon from './Components/index_component/icon.png';
 import Data from './Components/index_component/data.xml'
 
 function component () {
     let element = document.createElement('div');
+    let element2 = document.createElement('pre');
     let btn = document.createElement('button');
 
     btn.innerHTML = 'Click me';
@@ -18,6 +21,10 @@ function component () {
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    element2.innerHTML = [
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     
     // Добавление favicon`а на страницу
     document.querySelector("link[rel='shortcut icon'").href = Icon;
@@ -25,7 +32,7 @@ function component () {
     console.log(Data);
     element.appendChild(btn);
 
-    return element;
+    return element2;
 }
 
 //document.body.appendChild(component());
